7021686427b543ecdd3038bacf2861a298b56e6f
JannikRasch <jannikr21@gmail.com>
2023-04-18 23:58:04 +0200

    edit

--- tests/test_sport.py
+++ tests/test_sport.py
@@ -4,18 +4,18 @@ from unittest.mock import patch, ANY, mock_open
 @patch("builtins.open")
 @patch("os.path.exists")
 @patch("requests.get")
-def test_next_match(mock_requests, mock_exists, mock_open):
+def test_next_match(self, mock_requests, mock_exists, mock_open):
     obj = sport.SportAPI()
     
     obj.get_next_match()
-    mock_requests.assert_called_with("https://api.football-data.org/v2/competitions/BL1/matches")
+    mock_requests.assert_called_with(f"https://api.football-data.org/v2/competitions/BL1/matches")
 
 
 @patch("builtins.open")
 @patch("os.path.exists")
 @patch("requests.get")
-def test_current_matches(mock_requests, mock_exists, mock_open):
+def test_current_matches(self, mock_requests, mock_exists, mock_open):
     obj = sport.SportAPI()
     
     obj.get_current_matches()
-    mock_requests.assert_called_with("https://api.football-data.org/v2/matches?competitions=BL1&status=LIVE")
+    mock_requests.assert_called_with(f"https://api.football-data.org/v2/matches?competitions=BL1&status=LIVE")
